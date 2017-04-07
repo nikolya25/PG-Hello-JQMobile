2fa8b6093d28a9160271aa3cf48da9a7e3baf4c2
Nikolya <kolya_shchur@mail.ru>
2017-04-07 16:38:12 +0200

    popups added

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -18,3 +18,12 @@ function deviceInfo() {
 
 	document.getElementById("deviceDetails").innerHTML = info;	
 }
+
+$( document ).on( "pagecreate", function() {
+    $( ".photopopup" ).on({
+        popupbeforeposition: function() {
+            var maxHeight = $( window ).height() - 60 + "px";
+            $( ".photopopup img" ).css( "max-height", maxHeight );
+        }
+    });
+});
